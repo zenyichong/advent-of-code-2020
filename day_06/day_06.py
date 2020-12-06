648ad6ff05ef3e4440fb0b3896ee198105ee7a6e
zenyichong <zenyichong@gmail.com>
2020-12-06 19:50:02 +0800

    Day 6 solutions

---
+++ day_06/day_06.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+"""--- Advent of Code Day 6: Custom Customs ---"""
+
+from typing import List
+from functools import reduce
+
+FILENAME = "day_06.txt"
+
+
+def part1(inp: List[str]) -> int:
+    answers = [item.split() for item in inp]
+    count = 0
+    for group in answers:
+        count += len(set(''.join(group)))
+    return count
+
+
+def part2(inp: List[str]) -> int:
+    answers = [item.split() for item in inp]
+    count = 0
+    for group in answers:
+        group = [set(yes) for yes in group]
+        count += len(set.intersection(*group))
+    return count
+
+
+def main():
+    with open(FILENAME) as f:
+        inp = f.read().strip().split("\n\n")
+    print(f'Part 1: {part1(inp)}')
+    print(f'Part 2: {part2(inp)}')
+
+
+if __name__ == "__main__":
+    main()
