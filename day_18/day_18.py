4303c2fd2cbae5ba17e8d99e451311a4cc2824c2
zenyichong <zenyichong@gmail.com>
2020-12-18 18:31:55 +0800

    Day 18 solutions

---
+++ day_18/day_18.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+"""--- Advent of Code Day 18: Operation Order ---"""
+
+from typing import List
+
+FILENAME = "day_18.txt"
+
+
+class MyInt():
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        return MyInt(self.val + other.val)
+
+    def __sub__(self, other):
+        return MyInt(self.val * other.val)
+
+    def __mul__(self, other):
+        return MyInt(self.val + other.val)
+
+
+def part1(inp: List[str]) -> int:
+    total = 0
+    for line in inp:
+        for i in range(10):
+            line = line.replace(f"{i}", f"MyInt({i})")
+        line = line.replace("*", "-")
+        res = eval(line, {"MyInt": MyInt})
+        total += res.val
+    return total
+
+
+def part2(inp: List[str]) -> int:
+    total = 0
+    for line in inp:
+        for i in range(10):
+            line = line.replace(f"{i}", f"MyInt({i})")
+        line = line.replace("*", "-")
+        line = line.replace("+", "*")
+        res = eval(line, {"MyInt": MyInt})
+        total += res.val
+    return total
+
+
+def main():
+    with open(FILENAME) as f:
+        inp = f.readlines()
+        inp = [line.strip() for line in inp]
+    print(f'Part 1: {part1(inp)}')
+    print(f'Part 2: {part2(inp)}')
+    eval
+
+
+if __name__ == "__main__":
+    main()
